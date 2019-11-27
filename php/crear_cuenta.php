55598ac1cae68fade8283041947b52ce7f7626ae
root <root@pop-os.localdomain>
2019-11-27 12:20:51 -0500

    Encripta la segunda contraseña para verificación

--- php/crear_cuenta.php
+++ php/crear_cuenta.php
@@ -13,6 +13,7 @@ if(isset($_POST["submit"]))
          $password=$_POST["contra"];
          $rep_password=$_POST["ver_contra"];
          $password = md5($password);
+         $rep_password = md5($rep_password);
         
         Crear_Datos($nombre, $apellido, $cedula, $correo, $departamento, $password, $rep_password);
         header('Location:../index.html');
